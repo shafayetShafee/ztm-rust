a57ff614e62dad86a2bb0e1c9a34b63468eec6a3
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-16 14:41:14 +0600

    solved a1 and a2

--- activities/src/bin/a2.rs
+++ activities/src/bin/a2.rs
@@ -8,4 +8,17 @@
 // * Use a function to display the result
 // * Use the "{:?}" token in the println macro to display the result
 
-fn main() {}
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
+fn show_res(r: i32) {
+    println!("{:?}", r);
+}
+
+fn main() {
+    let x: i32 = 5;
+    let y: i32 = 11;
+    let res = add(x, y);
+    show_res(res);
+}
