d89e89f6effc8d244184a4747f021c6eae051d7e
shafayetShafee <sshafee@isrt.ac.bd>
2023-08-19 17:47:48 +0600

    completed tuple

--- activities/src/bin/a9.rs
+++ activities/src/bin/a9.rs
@@ -9,4 +9,17 @@
 // * Destructure the return value into two variables
 // * Use an if..else if..else block to determine what to print
 
-fn main() {}
+fn coord(x: f64, y: f64) -> (f64, f64) {
+    (x, y)
+}
+
+fn main() {
+    let (x, y) = coord(5.0, 10.0);
+    if y < 5.0 {
+        println!("y is less than 5");
+    } else if y > 5.0 {
+        println!("y is greater than 5");
+    } else {
+        println!("y is equal to 5");
+    }
+}
